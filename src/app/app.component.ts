5ca9a38772074208c8f804e5aade02291ca9a092
hafsamanzoordar <81043146+hafsamanzoordar@users.noreply.github.com>
2022-09-08 21:18:24 +0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'todoapp';
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,43 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
-import { ListManagerComponent } from './list-manager/list-manager.component';
-import { TodoListService } from './services/todo-list.service';
-import { StorageService } from './services/storage.service';
-import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
+  {path: 'servers', component: ServersComponent},
+  {path: 'servers/:id', component: ServerComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoItemComponent,
-    InputButtonUnitComponent,
-    ListManagerComponent
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [TodoListService, StorageService]
-  ,
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
